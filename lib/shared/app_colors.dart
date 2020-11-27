bc2b520ff8cebb9a30f2493a872a32c3a1042758
José Eduardo de S. Oliveira <eduardo.jeso@gmail.com>
2020-11-27 13:22:43 -0300

    Definição de tokens para interface

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color.fromARGB(12, 45, 46, 47);
+const Color primaryTextColor = Color.fromARGB(255, 45, 46, 47);
+const Color secondaryTextColor = Color.fromARGB(135, 45, 46, 47);
---
+++ lib/shared/ui_helpers.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UIHelper {
+  static const double _verticalSpaceMedium = 20.0;
+
+  static const double _horizontalSpaceMedium = 20.0;
+
+  static const Widget verticalSpaceMedium =
+      SizedBox(height: _verticalSpaceMedium);
+
+  static const Widget horizontalSpaceMedium =
+      SizedBox(width: _horizontalSpaceMedium);
+}
